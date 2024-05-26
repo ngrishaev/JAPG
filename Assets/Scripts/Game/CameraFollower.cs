86f5ee0a4c65747621ed714acf01c269e758e109
Nikita <grishaev.nk@gmail.com>
2024-05-26 02:49:43 +0200

    Camera follow

--- Assets/Scripts/Game/CameraFollower.cs
+++ Assets/Scripts/Game/CameraFollower.cs
@@ -4,5 +4,11 @@ namespace Game
 {
     public class CameraFollower : MonoBehaviour
     {
+        [SerializeField] private Transform _target = null!;
+
+        private void LateUpdate()
+        {
+            transform.position = new Vector3(_target.position.x, _target.position.y, transform.position.z);
+        }
     }
 }
