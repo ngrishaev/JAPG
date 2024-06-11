8914802a1244ca726446e39c0ad11f757bc3bf2c
Nikita <grishaev.nk@gmail.com>
2024-06-11 21:53:44 +0200

    Now creating really unique id for each id

--- Assets/Scripts/Editor/UniqueIdEditor.cs
+++ Assets/Scripts/Editor/UniqueIdEditor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Game;
 using UnityEditor;
 using UnityEditor.SceneManagement;
@@ -13,15 +14,29 @@ namespace Editor
         {
             var uniqueId = (UniqueId) target;
             
+            if(IsPrefab(uniqueId))
+                return;
+            
             if (string.IsNullOrEmpty(uniqueId.Id))
             {
                 SetNewId(uniqueId);
             }
+            else
+            {
+                var ids = FindObjectsOfType<UniqueId>();
+                if (ids.Any(other => other != uniqueId && other.Id == uniqueId.Id))
+                    SetNewId(uniqueId);
+            }
+        }
+
+        private bool IsPrefab(UniqueId uniqueId)
+        {
+            return uniqueId.gameObject.scene.rootCount == 0;
         }
 
         private void SetNewId(UniqueId uniqueId)
         {
-            uniqueId.Id = Guid.NewGuid().ToString();
+            uniqueId.Id = $"{uniqueId.gameObject.scene.name}_{Guid.NewGuid().ToString()}";
             if (Application.isPlaying)
                 return;
             
