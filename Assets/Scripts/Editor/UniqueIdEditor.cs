6bfd973a4c5f02754709af9d2c760694d02b9444
Nikita <grishaev.nk@gmail.com>
2024-06-11 20:34:53 +0200

    UniqueId

---
+++ Assets/Scripts/Editor/UniqueIdEditor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Game;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(UniqueId))]
+    public class UniqueIdEditor : UnityEditor.Editor
+    {
+        private void OnEnable()
+        {
+            var uniqueId = (UniqueId) target;
+            
+            if (string.IsNullOrEmpty(uniqueId.Id))
+            {
+                SetNewId(uniqueId);
+            }
+        }
+
+        private void SetNewId(UniqueId uniqueId)
+        {
+            uniqueId.Id = Guid.NewGuid().ToString();
+            if (Application.isPlaying)
+                return;
+            
+            EditorUtility.SetDirty(uniqueId);
+            EditorSceneManager.MarkSceneDirty(uniqueId.gameObject.scene);
+        }
+    }
+}
---
+++ Assets/Scripts/Game/UniqueId.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class UniqueId : MonoBehaviour
+    {
+        public string Id = null!;
+    }
+}
